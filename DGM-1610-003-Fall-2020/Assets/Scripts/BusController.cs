1ca3c89297acb148a3140abf4bfaf6002ea1f60e
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-09-09 17:17:18 -0600

    Created Script for Lesson 1.2 - Pedal to the Metal
    [Unit 01 - Player Movement] Lesson 1.2 - Pedal to the Metal

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/BusController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusController : MonoBehaviour
+{
+    public int velocity = 20;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Control Forward
+        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+    }
+}
