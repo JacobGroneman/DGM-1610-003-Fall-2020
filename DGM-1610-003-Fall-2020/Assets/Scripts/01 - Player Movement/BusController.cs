154469ec5c89a4bf4ec602b7e176b6628265f46a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-09-14 17:18:56 -0600

    Completed Lesson 1.3 - High Speed Chase
    [Unit 01 - Player Movement] Lesson 1.3 - High Speed Chase

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/01 - Player Movement/BusController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusController : MonoBehaviour
+{
+    public int velocity = 0;
+    public float turnSpeed = 0;
+    
+    public float HorizontalInput;
+    public float VerticalInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Control Button Mechanics
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+        
+        //Control Forward
+        transform.Translate(Vector3.forward * velocity * VerticalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * turnSpeed * HorizontalInput * Time.deltaTime);
+    }
+}
---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/01 - Player Movement/CameraFollowBus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowBus : MonoBehaviour
+{
+    public GameObject bus;
+    public Vector3 cameraOffset = new Vector3(0, 12, -12);
+
+    void Start()
+    {
+        bus = GameObject.Find("Bus");
+    }
+
+
+    void FixedUpdate()
+    {
+        var busPosition = bus.transform.position;
+        transform.position = busPosition + cameraOffset;
+    }
+}
