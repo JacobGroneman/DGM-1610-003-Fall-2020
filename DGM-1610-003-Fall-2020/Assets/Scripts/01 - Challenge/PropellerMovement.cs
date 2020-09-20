8b1712ab3573bbe5c6b5199c846510fe17eb6027
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-09-20 01:17:55 -0600

    Completed Challenge 1 - Plane Programming
    [Unit 01 - Player Movement] Challenge 1 - Plane Programming

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/01 - Challenge/PropellerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerMovement : MonoBehaviour
+{
+    private float turnSpeed = 500f;
+    
+    void Update()
+    {
+        #region Physics
+            //Rotates the Propeller
+                transform.Rotate(Vector3.back * turnSpeed * Time.deltaTime);
+                #endregion
+    }
+}
