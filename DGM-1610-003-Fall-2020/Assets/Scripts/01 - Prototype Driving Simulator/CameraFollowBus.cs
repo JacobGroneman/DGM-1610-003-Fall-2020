e4115025e7f970a71fb62eb646b3d70d73b98c7e
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-09-21 17:10:57 -0600

    Completed 2.1 - Player Positioning
    [Unit 02 - Basic Game Play] Lesson 2.1 - Player Positioning

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/01 - Prototype Driving Simulator/CameraFollowBus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowBus : MonoBehaviour
+{
+    private GameObject bus;
+    private Vector3 cameraOffset = new Vector3(0, 12, -12);
+
+    void Start()
+    {
+        bus = GameObject.Find("Bus");
+    }
+
+
+    void FixedUpdate()
+    {
+        //Camera Main Follow Bus Position
+        var busPosition = bus.transform.position;
+        transform.position = busPosition + cameraOffset;
+    }
+}
---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/02 - Prototype 2 - Animal Stamped/PlayerFarmerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFarmerController : MonoBehaviour
+{
+    private float _horizontalInput;
+    
+    void Update()
+    {
+        #region Input
+            _horizontalInput = Input.GetAxis("Horizontal");
+            #endregion
+    }
+}
