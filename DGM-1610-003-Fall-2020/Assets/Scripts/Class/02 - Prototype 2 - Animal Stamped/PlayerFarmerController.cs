98a4f343068da44def4221c8992eb6f1b3869663
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-09-23 16:56:08 -0600

    Started Lesson 2.2 - Food Flight
    [Unit 02 - Basic Game Play] Lesson 2.2 - Food Flight

--- DGM-1610-003-Fall-2020/Assets/Scripts/Class/02 - Prototype 2 - Animal Stamped/PlayerFarmerController.cs
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Class/02 - Prototype 2 - Animal Stamped/PlayerFarmerController.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class PlayerFarmerController : MonoBehaviour
 {
     private float _horizontalInput;
+    private float _velocity = 20f;
     
     void Update()
     {
         #region Input
+        //Horizontal Input
             _horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate
+                (Vector3.right * _horizontalInput * _velocity * Time.deltaTime);
+        // Horizontal Input Player Boundaries
+            if (transform.position.x >= 10)
+            {
+                transform.position = new Vector3
+                    (10, transform.position.y, transform.position.z);
+            }
+            if (transform.position.x <= -10)
+            {
+                transform.position = new Vector3
+                    (-10, transform.position.y, transform.position.z);
+            }
+            
+        //Space-bar Input Fish Launch!
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+            }
             #endregion
     }
 }
