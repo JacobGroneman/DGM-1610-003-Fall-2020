8314163dea48f53cb5413f46aa06445ea66afc8b
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-09-24 20:56:52 -0600

    Practiced Random Object Positioning

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Randomization/RandomPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    private GameObject[] _gameObjects;
+
+    void Start()
+    {
+        #region Assignment
+                _gameObjects = GameObject.FindGameObjectsWithTag("objs");
+                #endregion
+    }
+    
+    void Update()
+    {
+        #region Input
+            //Space-bar Positions _gameObjects[] Randomly
+                var randomValue = Random.Range(-100, 100);
+                if (!Input.GetKeyDown(KeyCode.Space)) return;
+                foreach (var obj in _gameObjects)
+                {
+                    obj.transform.position = new Vector3
+                        (randomValue, randomValue, randomValue);
+                }
+                #endregion
+    }
+}
