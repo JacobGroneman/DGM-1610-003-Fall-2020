f14dc50063b1f3c3d3c4bd8143326c042787dc08
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-09-25 21:23:29 -0600

    Unity Learn AI: Pursuit

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Unity Learn AI/Autonomously Moving Agents/Pursuit/AIBot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBot : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private GameObject _target;
+    
+    void Start()
+    {
+        _target = GameObject.Find("target");
+        _agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Pursue();
+    }
+
+    void Seek(Vector3 location)
+    {
+        _agent.SetDestination(location);
+    }
+
+    void Flee(Vector3 location)
+    {
+        Vector3 fleeVector = location - this.transform.position;
+        _agent.SetDestination(this.transform.position - fleeVector);
+    }
+
+    void Pursue()
+    {
+        Vector3 targetDirection = _target.transform.position - this.transform.position;
+
+        float relativeHeading = Vector3.Angle(this.transform.forward,
+            this.transform.TransformVector(_target.transform.forward));
+        float toTarget = Vector3.Angle
+            (this.transform.forward, this.transform.TransformVector(targetDirection));
+            
+        if (toTarget > 90 && relativeHeading < 20 ||_target.GetComponent<sfDrive>().currentSpeed < .01f)
+        {
+            Seek(_target.transform.position);
+            return;
+        }
+        
+        float lookAhead = 
+            targetDirection.magnitude /
+            (_agent.speed + _target.GetComponent<sfDrive>().currentSpeed);
+        Seek(_target.transform.position + _target.transform.forward * lookAhead);
+    }
+}
