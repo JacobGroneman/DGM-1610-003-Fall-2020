5a397b1bfa518fa826e71ff3fec3b65b313420f9
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-04 22:52:23 -0600

    Practiced If Statements, Randomization, and Methods

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Other/IfStatementExample.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatementExample : MonoBehaviour
+{
+    private int _randomNumber;
+    Color _resultColor;
+
+    void Start()
+    {
+        _resultColor = Color.clear;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _randomNumber = Random.Range(1, 10);
+            GenerateRandomizedResult(_randomNumber, _resultColor);
+        }
+    }
+
+    private static void GenerateRandomizedResult(int number, Color color)
+    {
+        if (number < 5)
+        {
+            color = Color.red;
+        }
+        else if(number == 5)
+        {
+            color = Color.yellow;
+        }
+        else if (number > 5)
+        {
+            color = Color.green;
+        }
+    }
+}
