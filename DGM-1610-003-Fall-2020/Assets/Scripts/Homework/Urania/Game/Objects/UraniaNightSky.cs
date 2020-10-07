52ae4513159a5b579468d575f2293f928f9ca181
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-06 19:25:37 -0600

    Urania: Creating Script Lists and Assigning Variables

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Urania/Game/Objects/UraniaNightSky.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UraniaObject
+{
+    public string 
+        name;
+
+    public Vector3
+        location;
+
+    public Sprite
+        sprite;
+}
+
+public class UraniaNightSky : MonoBehaviour
+{
+    //Organized By Distance
+    public List<HomeStar> HomeStars;
+    public List<HomeMoon> HomeMoons;
+    public List<Satellite> Satellites;
+    
+    public List<Planet> Planets;
+    public List<Comet> Comets;
+    public List<Asteroid> Asteroids;
+    
+    public List<Star> Stars;
+    public List<Nebula> Nebulae;
+    public List<BlackHole> BlackHoles;
+    
+    //Outside System
+    public List<Galaxy> Galaxies;
+    
+    //Anonymous
+    public List<UAO> UAOs; //Unidentified Astronomical Object
+    public List<Other> Others;
+    public List<Creature> Creatures;
+}
+
+public class HomeStar : UraniaObject
+{
+   
+}
