6c4853ead1ca89223e9bcd67eaa3e58910d13171
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-08 22:40:34 -0600

    Practiced CoRoutine Radio Scanner Automation

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/General Learning/CoRoutines/SomeCoRoutineScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script simulates the scanning radio bands from 0Hz to 3,000GHz,
+//starting from a random frequency and raising .10Hz every half-second
+//until 3,000GHz.
+
+public class SomeCoRoutineScript : MonoBehaviour
+{
+    private float _frequency, _randomFrequency;
+
+    void Start()
+    {
+        _randomFrequency = Random.Range(0f, 3000000000000f);
+        StartCoroutine(FrequencyScan());
+    }
+
+    private IEnumerator FrequencyScan()
+    {
+        do
+        {
+            _frequency += .10f;
+            yield return new WaitForSeconds(0.5f);
+        } while (_frequency < 3000000000000f);
+    }
+}
