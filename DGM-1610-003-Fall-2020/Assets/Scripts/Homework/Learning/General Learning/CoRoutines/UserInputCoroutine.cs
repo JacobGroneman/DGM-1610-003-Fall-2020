2d88431434d6ad4d7e7f5c1362a60e9c295c2cb8
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-09 22:53:57 -0600

    Practiced a User-Input Charge CoRoutine

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/General Learning/CoRoutines/UserInputCoroutine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputCoroutine : MonoBehaviour
+{
+    private Collider _upgradeCollider;
+
+    private float _chargeInterval = 1.0f;
+    private int _chargeLoadAmount = 1;
+    private int _percentageCharged;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _percentageCharged <= 100)
+        {
+            StartCoroutine(ChargeOnInput());
+        }
+
+        if (_upgradeCollider.isTrigger)
+        {
+            _chargeInterval -= 0.01f;
+        }
+    }
+
+    IEnumerator ChargeOnInput()
+    {
+        do
+        {
+            _percentageCharged += _chargeLoadAmount;
+            yield return new WaitForSeconds(_chargeInterval);
+        } while (_percentageCharged > 100);
+    }
+}
