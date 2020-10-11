7d92536ebe92311c011fc4de85e499ad8d7f0c1b
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-11 01:23:21 -0600

    Completed Challenge 2 - Play Fetch
    [Unit 02 - Basic Game Play] Challenge 2 - Play Fetch

---
+++ DGM-1610-003-Fall-2020/Assets/Scenes and Challenges/Challenge 2/Scripts/DogPlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class DogPlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool spawnIsWait = false; 
+
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.timeScale != 0f && spawnIsWait == false)
+        {
+            spawnIsWait = true;
+            StartCoroutine(SendDog());
+        }
+    }
+
+    //Prevents spawning multiple dogs
+    IEnumerator SendDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        yield return new WaitForSeconds(2f);
+        spawnIsWait = false;
+    }
+}
---
+++ DGM-1610-003-Fall-2020/Assets/Scenes and Challenges/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*NOTICE!
+ Spawn interval did not work alongside InvokeRepeating() alone. 
+ CancelInvoke() and Invoke() therefore creates another loop to compensate
+ for this error*/
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        //Cancels InvokeRepeating after execution, cancels sequentially-run invokes after complete execution
+        CancelInvoke();
+        
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        int ballLength = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[ballLength], spawnPos, ballPrefabs[ballLength].transform.rotation);
+        
+        //Change spawn interval to 3-5 seconds and re-Invokes the method.
+        spawnInterval = Random.Range(3.0f, 5.0f);
+        Invoke("SpawnRandomBall", spawnInterval);
+    }
+}
