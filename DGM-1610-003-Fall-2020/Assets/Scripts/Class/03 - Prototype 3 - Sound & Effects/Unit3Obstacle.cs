297a7870d0b47eb1e1909588b557c44160682075
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-12 17:26:10 -0600

    Completed Lesson 3.1 - Jump Force
    [Unit 03 - Sound & Effects] Lesson 3.1 - Jump Force

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Class/03 - Prototype 3 - Sound & Effects/Unit3Obstacle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit3Obstacle : MonoBehaviour
+{
+    public float velocity;
+    
+    void FixedUpdate()
+    {
+        transform.Translate
+            (Vector3.left * velocity * Time.deltaTime);
+    }
+}
---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Class/03 - Prototype 3 - Sound & Effects/Unit3PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Player is Floating
+public class Unit3PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public int _jumpPower = 6;
+    public float _gravity = -9.8f;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        Physics.gravity *= _gravity;
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+        }
+    }
+}
