5cf515869ee383682781a7922c6f63ae238d11d1
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-20 22:29:05 -0600

    Practiced DoWhile Loops

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Loops/PracticeDoWhile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PracticeDoWhile : MonoBehaviour
+{
+    private int _points;
+    private bool _isGrounded = true;
+    public AudioSource GroundedAudio, AirbourneAudio;
+    
+    void Update()
+    {
+        do
+        {
+            _points++;
+        } while (_isGrounded == true);
+        do
+        {
+            _points--;
+        } while (_isGrounded == false);
+        
+
+        if (_isGrounded == true)
+        {
+            AirbourneAudio.mute = true;
+            GroundedAudio.mute = false;
+            GroundedAudio.Play();
+        }
+        else if (_isGrounded == false)
+        {
+            GroundedAudio.mute = true;
+            AirbourneAudio.mute = false;
+            AirbourneAudio.Play();
+        }
+    }
+}
