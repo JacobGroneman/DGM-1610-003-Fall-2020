4fef8208e2d9fb72246420b151e004df93f20e35
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-24 23:00:08 -0600

    Practiced Declaration of C# Scripts

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/ScriptSharing/PracticeScriptAuthor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeScriptAuthor : MonoBehaviour
+{
+    public float velocity = 5;
+    private float _storedVelocity = 0;
+
+    void Start()
+    {
+        _storedVelocity = velocity;
+    }
+
+    void Update()
+    {
+        _storedVelocity = velocity;
+    }
+}
---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/ScriptSharing/PracticeScriptReferencer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeScriptReferencer : MonoBehaviour
+{
+    private PracticeScriptAuthor _speedScript;
+
+    void Update()
+    {
+        _speedScript.velocity = transform.position.magnitude / Time.deltaTime;
+
+        if (_speedScript.velocity == 0)
+        {
+            Debug.Log("Throttle It!");
+        }
+        else if(_speedScript.velocity >= 100)
+        {
+            Debug.Log("Slow It, Buster!");
+        }
+    }
+}
