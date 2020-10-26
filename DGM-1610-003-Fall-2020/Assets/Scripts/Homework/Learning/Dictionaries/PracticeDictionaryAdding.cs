494d13de5ebbd09df6e0edc7b35a046ade645f52
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-25 21:26:34 -0600

    Reviewed Dictionaries

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Dictionaries/PracticeDictionaryAdding.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem
+{
+    public string itemName;
+    public int itemID;
+}
+
+public class PracticeDictionaryAdding : MonoBehaviour
+{
+    public Dictionary<int, InventoryItem> Inventory 
+        = new Dictionary<int, InventoryItem>();
+
+    public InventoryItem
+        wallet, keys, phone, gum;
+    
+    public int time = 9000;
+
+    void Start()
+    {
+        wallet.itemName = "Wallet";
+        wallet.itemID = 0;
+        keys.itemName = "Keys";
+        wallet.itemID = 1;
+        phone.itemName = "Phone";
+        phone.itemID = 2;
+        gum.itemName = "Gum";
+        gum.itemID = 3;
+    }
+    
+    void Update()
+    {
+        if (time == 9050)
+        {
+            Inventory.Add(wallet.itemID, wallet);
+            Inventory.Add(keys.itemID, keys);
+            Inventory.Add(phone.itemID, phone);
+            Inventory.Add(gum.itemID, gum);
+        }
+    }
+}
