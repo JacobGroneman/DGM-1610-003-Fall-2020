45c2fc2fa00d72bcf20f266e6789533d59036b5a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-10-29 21:50:15 -0600

    Practiced Get/Set Properties

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/GetSet/PracticeGetSet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeGetSet : MonoBehaviour
+{
+    private int _score;
+    private int _highScore;
+
+    public int Score
+    {
+        get => _score;
+        set { if (value == _highScore) {UnityEngine.Debug.Log
+                ("You set a new high score!");} 
+            _score = value;}
+    }
+
+    void Update()
+    {
+        if (_score > _highScore)
+        {
+            _highScore = _score;
+        }
+    }
+}
