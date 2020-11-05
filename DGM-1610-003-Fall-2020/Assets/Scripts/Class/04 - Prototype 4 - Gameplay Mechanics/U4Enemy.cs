02688856264b86ebf9e05d065607e34d23d6d426
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-04 17:21:14 -0700

    Completed Lesson 4.1 - Watch Where You're Going!
    [Unit 04 - Gameplay Mechanics] - Lesson 4.1 - Watch Where You're Going!

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Class/04 - Prototype 4 - Gameplay Mechanics/U4Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U4Enemy : MonoBehaviour
+{
+    public float _velocity = 10f;
+    
+    private Rigidbody _rigidbody;
+
+    private GameObject _avatar;
+    
+    void Start()
+    {
+        #region Assignment
+            _rigidbody = GetComponent<Rigidbody>();
+            _avatar = GameObject.Find("Avatar");
+            #endregion
+    }
+
+    void Update()
+    {
+        #region Movement
+            _rigidbody.AddForce
+                ((_avatar.transform.position - transform.position).normalized * _velocity);
+            #endregion
+       
+    }
+}
---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Class/04 - Prototype 4 - Gameplay Mechanics/U4RotateCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U4RotateCamera : MonoBehaviour
+{
+    
+    public float rotationalVelocity = 50f;
+
+    void Update()
+    {
+        #region Input
+        //Rotates camera around the Y axis
+            float horizontalInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up, horizontalInput * rotationalVelocity * Time.deltaTime);
+            #endregion
+    }
+}
