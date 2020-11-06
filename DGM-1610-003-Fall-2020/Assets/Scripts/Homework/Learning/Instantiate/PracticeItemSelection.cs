1d2d275541e04034fe75d06533b9a6db4714486a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-05 20:45:42 -0700

    Practiced Item Adding

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Instantiate/PracticeItemSelection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class PracticeItemSelection : MonoBehaviour
+{
+    public Dictionary<string, GameObject> inventory;
+
+    public GameObject addedObject;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(inventory["Axe"], Vector3.zero, Quaternion.identity);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision");
+        
+        if (other.gameObject.CompareTag("Item"))
+        {
+            addedObject = GameObject.Find(other.name);
+            inventory.Add(other.gameObject.name, addedObject);
+        }
+        
+        //I Got to Research this Stuff
+    }
+}
