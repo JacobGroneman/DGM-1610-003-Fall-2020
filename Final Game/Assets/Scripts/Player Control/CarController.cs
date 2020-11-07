921b269ef34a57286ef7a233346e6b097ad2b144
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-07 00:07:25 -0700

    Final Game: Tutorial Car Controller Script

--- Final Game/Assets/Scripts/Player Control/CarController.cs
+++ Final Game/Assets/Scripts/Player Control/CarController.cs
@@ -6,19 +6,47 @@ public class CarController : MonoBehaviour
 {
     public float forwardVelocity;
     public float rotationalVelocity;
+
+    public bool _isGrounded;
     
     void Update()
     {
         #region Input
                         //Replace -vertical With a Reverse Mechanism
         //Forward
-            float translation = Input.GetAxis("Vertical");
-            float translationParameters = translation * forwardVelocity * Time.deltaTime;
+            float inputVertical = Input.GetAxis("Vertical");
+            float translationParameters = inputVertical * forwardVelocity * Time.deltaTime;
             transform.Translate(Vector3.forward * translationParameters);
         //Left/Right
-            float rotation = Input.GetAxis("Horizontal");
-            float rotationParameters = rotation * forwardVelocity * Time.deltaTime;
+            float inputHorizontal = Input.GetAxis("Horizontal");
+            float rotationParameters = inputHorizontal * rotationalVelocity * Time.deltaTime;
             transform.Rotate(Vector3.up * rotationParameters);
-        #endregion
+            #endregion
+            
+        #region Physical Parameters
+        //Car velocities relational to physical ground
+        // *See OnCollisionEnter
+            if (!_isGrounded) 
+            { 
+                forwardVelocity = 0f; 
+                rotationalVelocity = 0f;
+            }
+            else if (_isGrounded)
+            {
+                forwardVelocity = 150f; 
+                rotationalVelocity = 100f;
+            }
+            #endregion
     }
+
+    #region Unity Methods
+        void OnCollisionEnter(Collision other)
+        {
+            //Terrain Collisions
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                _isGrounded = true;
+            }
+        }
+        #endregion
 }
