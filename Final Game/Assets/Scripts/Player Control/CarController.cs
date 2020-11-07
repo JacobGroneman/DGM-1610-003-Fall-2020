646a719306d1eb6d4a1a174b700f18dcf11fafd6
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-06 22:35:11 -0700

    Final Game: Updating the Controller

---
+++ Final Game/Assets/Scripts/Player Control/CarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float forwardVelocity;
+    public float rotationalVelocity;
+    
+    void Update()
+    {
+        #region Input
+                        //Replace -vertical With a Reverse Mechanism
+        //Forward
+            float translation = Input.GetAxis("Vertical");
+            float translationParameters = translation * forwardVelocity * Time.deltaTime;
+            transform.Translate(Vector3.forward * translationParameters);
+        //Left/Right
+            float rotation = Input.GetAxis("Horizontal");
+            float rotationParameters = rotation * forwardVelocity * Time.deltaTime;
+            transform.Rotate(Vector3.up * rotationParameters);
+        #endregion
+    }
+}
