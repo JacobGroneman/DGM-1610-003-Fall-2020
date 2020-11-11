c7b14d796cea4b579e3df23a4cbd15c97dfbb0ab
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-10 23:16:13 -0700

    Practiced Random Material Color Change Based on Input

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/If Statements/PracticeSimpleColorChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeSimpleColorChange : MonoBehaviour
+{
+    private GameObject _colorOrb;
+
+    private Color _color;
+    
+    void Start()
+    {
+        #region Assignment
+            _colorOrb = GameObject.Find("Color Orb");
+            #endregion
+    }
+
+    void Update()
+    {
+        #region Input
+        //Changes _colorOrb Material Color Randomly
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                RandomChangeColor();
+            }
+            #endregion
+    }
+
+    private Color RandomChangeColor()
+    {//Changes _colorOrb Material Color Randomly
+        _color = new Color
+            (Random.value, Random.value, Random.value, Random.value);
+        _colorOrb.GetComponent<MeshRenderer>().material.color = _color;
+
+        return _color;
+    }
+}
