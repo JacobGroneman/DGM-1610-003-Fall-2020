6ca622cfd444805595b6fb0aafab58facfcbfce9
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-11 17:21:28 -0700

    Completed Lesson 4.3 -  PowerUp and CountDown
    [Unit 04 - Gameplay Mechanics] - Lesson 4.3 -  PowerUp and CountDown

--- DGM-1610-003-Fall-2020/Assets/Scripts/Class/04 - Prototype 4 - Gameplay Mechanics/U4PlayerController.cs
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Class/04 - Prototype 4 - Gameplay Mechanics/U4PlayerController.cs
@@ -9,6 +9,11 @@ public class U4PlayerController : MonoBehaviour
     private Rigidbody _rb;
 
     private GameObject _focalPoint;
+    public GameObject powerupIndicator;
+    
+    // Power-ups
+        private bool _hasPowerup = false;
+        private float _powerupStrength = 15.0f;
     
     void Start()
     {
@@ -26,5 +31,48 @@ public class U4PlayerController : MonoBehaviour
             var forceParameters = inputForward * _velocity;
             _rb.AddForce(_focalPoint.transform.forward * forceParameters);
             #endregion
+
+        #region Placement
+        //Places Power-up Indicator Below Player                
+            powerupIndicator.transform.position = 
+            transform.position + new Vector3(0, -0.5f, 0);
+            #endregion
     }
+
+    #region Collider
+    //Power-up Attainment and Countdown
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Powerup"))
+            {
+                _hasPowerup = true;
+                Destroy(other.gameObject);
+                powerupIndicator.gameObject.SetActive(true);
+                StartCoroutine(PowerupCountdown());
+            }
+        }
+    //Makes Enemies Fly after Player Attains Power-up
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Enemy") && _hasPowerup)
+            { 
+                /*Debug.Log("Player Collision: " + collision.gameObject
+                 + ". HasPowerUp =  " + _hasPowerup);*/
+                
+                Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+                Vector3 vectorCollision = collision.gameObject.transform.position - transform.position;
+                
+                enemyRb.AddForce(vectorCollision * _powerupStrength, ForceMode.Impulse);
+            }
+        }
+    #endregion
+
+    #region Routine
+        IEnumerator PowerupCountdown()
+        {
+            yield return new WaitForSeconds(7);
+            _hasPowerup = false;
+            powerupIndicator.gameObject.SetActive(false);
+        }
+        #endregion
 }
