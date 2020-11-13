cdb4fc7c2e6efee63cc23a0bee8021457243d083
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-12 22:46:55 -0700

    Practiced Binary Formidable Saving Systems

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Save Files/PracticeSaveData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] //Enables Binary Serialization!!
+public class PracticeSaveData : MonoBehaviour
+{
+    #region Singleton
+        private static PracticeSaveData _current; 
+        public static PracticeSaveData Current
+        {
+            get
+            {
+                if (_current == null)
+                {_current = new PracticeSaveData();} 
+                return Current;
+            }
+        }
+        #endregion
+        
+        //https://www.youtube.com/watch?v=5roZtuqZyuw
+        //[3:47]
+}
