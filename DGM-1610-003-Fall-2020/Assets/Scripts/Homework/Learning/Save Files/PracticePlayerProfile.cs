93ff24898ef1ed812ffa813ac9aa4b9fc2e61c35
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-13 22:58:39 -0700

    Practiced Storing Data Using a Binary Formidable Save System

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Save Files/PracticePlayerProfile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PracticePlayerProfile : MonoBehaviour
+{
+    public string palyerName = "MountHammySandwich_45";
+    public int moneys = 0;
+    public int experienceLevel = 0;
+}
--- DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Save Files/PracticeSaveData.cs
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Save Files/PracticeSaveData.cs
@@ -17,7 +17,51 @@ public class PracticeSaveData : MonoBehaviour
             }
         }
         #endregion
+
+        public PracticePlayerProfile PlayerProfile;
         
+        public int BoughtWeapons;
+        public int BoughtTrophys;
+
+        #region Loading
+            public void OnLoadGame()
+            {
+                PracticeSaveData._current = (PracticeSaveData) //May be Current
+                    PracticeSerializationManager.Load(Application.persistentDataPath + "/saves/Save.save");
+            }
+            #endregion
+
+    public void GetMoneys()
+        {
+            PracticeSaveData.Current.PlayerProfile.moneys += 100;
+            PracticeSaveData.Current.PlayerProfile.experienceLevel += 1;
+        }
+
+    #region Shopping
+    
+        public void BuyWeapon()
+            {
+                if (PracticeSaveData.Current.PlayerProfile.moneys >= 200)
+                {
+                    PracticeSaveData.Current.PlayerProfile.moneys -= 200;
+                    PracticeSaveData.Current.BoughtWeapons += 1;
+                    //UpdateUI();
+                }
+            }
+    
+            public void BuyTrophy()
+            {
+                if (PracticeSaveData.Current.PlayerProfile.moneys >= 350)
+                {
+                    PracticeSaveData.Current.PlayerProfile.moneys -= 350;
+                    PracticeSaveData.Current.BoughtTrophys += 1;
+                    //UpdateUI();
+                }
+            }
+            #endregion
+        
+        
+
         //https://www.youtube.com/watch?v=5roZtuqZyuw
-        //[3:47]
+        //[5:34]
 }
