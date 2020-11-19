0e3ab6dca362c38512054776f6510a461cd24e59
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-18 22:20:26 -0700

    Practiced Spawning and Collisions, World Building Final Game

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Spawning/PracticeAlien.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeAlien : MonoBehaviour
+{
+    private ParticleSystem _smokeParticle;
+    
+    private AudioSource _audioSource;
+    private AudioClip _squeal;
+    private AudioClip _yell;
+
+    void Start()
+    {
+        #region Assignment
+            _smokeParticle = GameObject.Find("SmokeParticle")
+                .GetComponent<ParticleSystem>();
+    
+            _audioSource = GetComponent<AudioSource>();
+            _squeal = GetComponent<AudioSource>().clip;
+            #endregion
+    }
+
+    #region Collisions
+        void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.CompareTag("Laser"))
+            {
+                //Smoke Particle
+                _smokeParticle.gameObject.transform.position 
+                    = this.transform.position;
+                other.gameObject.GetComponent<ParticleSystem>().Play();
+                //"I'm Dying!"
+                _yell = GetComponent<AudioSource>().clip;
+                _audioSource.Play();
+                //Bye-Bye!
+                Destroy(other.gameObject);
+            }
+        }
+        #endregion
+}
---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Spawning/PracticeSpawnProgression.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeSpawnProgression : MonoBehaviour
+{
+    public int AlienCount;
+    private float _spawnRange = 100;
+    public int SpawnNumberAlien = 1;
+    public GameObject AlienPrefab;
+
+    void Start()
+    {
+        StartSpawn(SpawnNumberAlien, AlienPrefab);
+    }
+
+    void Update()
+    {
+        AlienCount = FindObjectsOfType<PracticeAlien>().Length;
+        if (AlienCount == 0)
+        {
+            SpawnNumberAlien++;
+            StartSpawn(SpawnNumberAlien, AlienPrefab);
+        }
+    }
+
+    #region Spawn
+    //Generates a Random Spawn Position
+        private Vector3 _spawnPositionRandom()
+        {
+            float spawnPosX = Random.Range(-_spawnRange, _spawnRange);
+            float spawnPosZ = Random.Range(-_spawnRange, _spawnRange);
+            Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+                
+            return randomPos;
+        }
+    //Spawns Creatures
+        private void StartSpawn(int spawnAmount, GameObject creatureToSpawn)
+        {
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                Instantiate
+                    (creatureToSpawn, _spawnPositionRandom(), AlienPrefab.transform.rotation);
+            }
+        }
+        #endregion
+}
