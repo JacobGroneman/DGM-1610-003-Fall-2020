874040748b0faee09d25c12aaee4027bf5717c38
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-19 23:52:45 -0700

    Practiced Simple Traffic Switch Controller

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/Switches/PracticeTrafficSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class PracticeTrafficSwitch : MonoBehaviour
+{
+    private GameObject _trafficLight;
+    
+    private int _colorIndex = 0;
+
+    void Start()
+    {
+        _trafficLight.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && _colorIndex >= 0)
+        {
+            _colorIndex--;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && _colorIndex <= 2)
+        {
+            _colorIndex++;
+        }
+        
+        Traffic();
+    }
+
+    void Traffic()
+    {
+        Color trafficColor = _trafficLight.GetComponent<MeshRenderer>().material.color;
+
+        switch (_colorIndex)
+        {
+            case 0:
+                trafficColor = Color.red;
+                break;
+            case 1:
+                trafficColor = Color.yellow;
+                break;
+            case 2:
+                trafficColor = Color.green;
+                break;
+        }
+    }
+}
