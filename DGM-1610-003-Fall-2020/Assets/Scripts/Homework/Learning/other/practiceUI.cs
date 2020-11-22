fc6278a4d61d93f3dd84929f965d5239af6e5444
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-21 20:49:50 -0700

    Exploring More Classes and Methods

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Learning/other/practiceUI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class practiceUI : MonoBehaviour
+{
+    private Image _image;
+
+    void Start()
+    {
+        GUI.color = Color.black;
+        
+        Matrix4x4 matrix4X4 = GUI.matrix;
+        matrix4X4.m00 = Single.Epsilon;
+
+        float _infiniti = Mathf.Infinity;
+
+        var length = Uri.UriSchemeHttps.Length;
+
+        var  _batteryStatus = BatteryStatus.Charging;
+        
+        Skybox.Destroy(this.gameObject);
+
+        var b = Texture.anisotropicFiltering == AnisotropicFiltering.Disable;
+        
+        WindZone.Destroy(gameObject); //Research these 
+    }
+}
