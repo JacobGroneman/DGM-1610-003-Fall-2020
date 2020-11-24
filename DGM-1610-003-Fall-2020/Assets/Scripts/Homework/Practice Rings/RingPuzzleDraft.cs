406b8d987afd4ad971265a995711f1f77d3c94c3
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-23 23:28:35 -0700

    Final Game: Initialized Position and Debugging Held Disk

--- DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Practice Rings/RingPuzzleDraft.cs
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Practice Rings/RingPuzzleDraft.cs
@@ -13,24 +13,38 @@ public class RingPuzzleDraft : MonoBehaviour
 
     private List<GameObject> _heldDisk;
 
+    private Vector3 _1RingPos = new Vector3
+        (-12, 2.99798298f, 0);
+    private Vector3 _2RingPos = new Vector3
+        (-12, 2.12836f, 0);
+    private Vector3 _3RingPos = new Vector3
+        (-12, 1.257478f, 0);
+    private Vector3 _4RingPos = new Vector3
+        (-12, 0.3917079f, 0);
+    private Vector3 _5RingPos = new Vector3
+        (-12, -0.4753096f, 0);
+    
     void Start()
     {
         #region Initialize
-
-        _1Ring = GameObject.Find("Ring one");
-        _2Ring = GameObject.Find("Ring two");
-        _3Ring = GameObject.Find("Ring three");
-        _4Ring = GameObject.Find("Ring four");
-        _5Ring = GameObject.Find("Ring five");
+            _1Ring = GameObject.Find("Ring one");
+            _2Ring = GameObject.Find("Ring two");
+            _3Ring = GameObject.Find("Ring three");
+            _4Ring = GameObject.Find("Ring four");
+            _5Ring = GameObject.Find("Ring five");
     
-        Peg1.Add(_5Ring);
-        Peg1.Add(_4Ring);
-        Peg1.Add(_3Ring);
-        Peg1.Add(_2Ring);
-        Peg1.Add(_1Ring);
-
-        /* Initialize Disk Positions */
-        #endregion
+            _1Ring.transform.position = _1RingPos;
+            _2Ring.transform.position = _2RingPos;
+            _3Ring.transform.position = _3RingPos;
+            _4Ring.transform.position = _4RingPos;
+            _5Ring.transform.position = _5RingPos;
+            
+            Peg1.Add(_5Ring);
+            Peg1.Add(_4Ring);
+            Peg1.Add(_3Ring);
+            Peg1.Add(_2Ring);
+            Peg1.Add(_1Ring);
+            #endregion
     }
     void Update()
     {
--- DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Practice Rings/RingPuzzleTest.cs
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Practice Rings/RingPuzzleTest.cs
@@ -5,41 +5,67 @@ using UnityEngine;
 
 public class RingPuzzleTest : MonoBehaviour
 {
-    public class Ring
-    {
-        public bool IsOnTop;
-        public GameObject obj;
-    }
+    public List<GameObject> Rings;
+    public List<GameObject> PegOne;
+    public List<GameObject> PegTwo;
+    public List<GameObject> PegThree;
+
+    public GameObject _1Ring,_2Ring, _3Ring, _4Ring, _5Ring;
+
+    private Vector3 _1RingPos = new Vector3
+        (-12, 2.99798298f, 0);
+    private Vector3 _2RingPos = new Vector3
+        (-12, 2.12836f, 0);
+    private Vector3 _3RingPos = new Vector3
+        (-12, 1.257478f, 0);
+    private Vector3 _4RingPos = new Vector3
+        (-12, 0.3917079f, 0);
+    private Vector3 _5RingPos = new Vector3
+        (-12, -0.4753096f, 0);
     
-    public List<Ring> Peg1;
-    public List<Ring> Peg2;
-    public List<Ring> Peg3;
-
-    public Ring _1Ring, _2Ring, _3Ring, _4Ring, _5Ring;
-
-    private List<GameObject> _heldDisk;
-
+    public List<GameObject> _heldRing;
+    
     void Start()
     {
         #region Initialize
-
-        _1Ring.obj = GameObject.Find("Ring one");
-        _2Ring.obj = GameObject.Find("Ring two");
-        _3Ring.obj = GameObject.Find("Ring three");
-        _4Ring.obj = GameObject.Find("Ring four");
-        _5Ring.obj = GameObject.Find("Ring five");
-    
-        Peg1.Add(_5Ring);
-        Peg1.Add(_4Ring);
-        Peg1.Add(_3Ring);
-        Peg1.Add(_2Ring);
-        Peg1.Add(_1Ring);
-/* Initialize Disk Positions */
-        #endregion
+        //Assigns Rings to Variables
+            _1Ring = GameObject.Find("Ring one");
+            _2Ring = GameObject.Find("Ring two");
+            _3Ring = GameObject.Find("Ring three");
+            _4Ring = GameObject.Find("Ring four");
+            _5Ring = GameObject.Find("Ring five");
+        //Sets Ring Positions
+            _1Ring.transform.position = _1RingPos;
+            _2Ring.transform.position = _2RingPos;
+            _3Ring.transform.position = _3RingPos;
+            _4Ring.transform.position = _4RingPos;
+            _5Ring.transform.position = _5RingPos;
+        //Adds Rings to Peg 1
+            PegOne.Add(_5Ring);
+            PegOne.Add(_4Ring);
+            PegOne.Add(_3Ring);
+            PegOne.Add(_2Ring);
+            PegOne.Add(_1Ring);
+        //Adds Rings to Rings List for operations
+            foreach (var ring in PegOne)
+            {
+                Rings.Add(ring);
+            }
+            #endregion
     }
 
     void Update()
     {
-        
+        //Check Whether Ring is on Top
+            /*foreach (var ring in Rings)
+            {
+                if (ring == PegOne.Last() 
+                    || ring == PegTwo.Last()
+                    || ring == PegThree.Last())
+                {
+                    //If clicked on:
+                    _heldRing.Add(ring);
+                }
+            }*/
     }
 }
