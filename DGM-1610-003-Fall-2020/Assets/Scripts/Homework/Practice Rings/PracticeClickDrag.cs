525ec4a8642b0100c049c8a376950d3f11064db1
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-25 21:23:13 -0700

    Final Game: Referenced a Click-Drag Script

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Homework/Practice Rings/PracticeClickDrag.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeClickDrag : MonoBehaviour
+{
+    private Vector3 _mouseOffset;
+
+    private float _mouseZCoordinate;
+    
+    void OnMouseDown()
+    {
+        _mouseZCoordinate =
+            Camera.main.WorldToScreenPoint
+                (gameObject.transform.position).z;
+        _mouseOffset = 
+            gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        //Pixel Coordinate System
+            Vector3 mousePoint = Input.mousePosition;
+        //Z Coordinate is the Game Object's
+            mousePoint.z = _mouseZCoordinate;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+    
+    void OnMouseDrag()
+    {
+        transform.position =
+            GetMouseWorldPosition() + _mouseOffset;
+    }
+    
+}
