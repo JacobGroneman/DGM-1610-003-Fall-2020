66ac16b40d2a3fb1ced5ef6533919b3cec952fa6
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-11-30 17:54:22 -0700

    Completed Lesson 5.1 - Clicky Mouse
    [Unit 05 - User Interface] - Lesson 5.1 - Clicky Mouse

---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Class/05 - Prototype 5 - User Interface/U5Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class U5Manager : MonoBehaviour
+{
+    public List<GameObject> Targets;
+
+    private float _spawnInterval = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_spawnInterval);
+            
+            int index = Random.Range(0, Targets.Count);
+            Instantiate(Targets[index]);
+            
+            //yield break;
+        }
+    }
+}
---
+++ DGM-1610-003-Fall-2020/Assets/Scripts/Class/05 - Prototype 5 - User Interface/U5Target.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U5Target : MonoBehaviour
+{
+    private float
+        _minVelocity = 12, _maxVelocity = 16,
+        _maxTorque = 10,
+        _xRange = 4, _ySPawnPos = 6; 
+
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.AddForce(RandomForce(), ForceMode.Impulse);
+        _rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #region Interactions
+        void OnMouseDown()
+        {
+            Destroy(gameObject);
+        }
+    
+        private void OnTriggerEnter(Collider other)
+        {
+            Destroy(gameObject);
+        }
+        #endregion
+
+    #region Attitude
+         Vector3 RandomSpawnPos()
+         {
+             return new Vector3(Random.Range(-_xRange, _xRange), _ySPawnPos);
+         }
+        
+         Vector3 RandomForce()
+         {
+             return Vector3.up * Random.Range(-_maxTorque, _maxTorque);
+         }
+        
+         float RandomTorque()
+         {
+             return Random.Range(-_maxTorque, _maxTorque);
+         }
+         #endregion
+}
